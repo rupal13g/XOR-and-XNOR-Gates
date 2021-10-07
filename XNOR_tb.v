a96bcb25d81065a574930c776613c48c04cc0b7c
Rupal Gupta <52855649+rupal13g@users.noreply.github.com>
2021-10-07 10:37:04 +0530

    Create XNOR_tb.v

---
+++ XNOR_tb.v
@@ -0,0 +1,20 @@
+module xnor_tb;
+  reg ta, tb;
+  wire ty;
+  xnorgate xnog(.a(ta), .b(tb), .y(ty));
+  initial
+    begin
+      $monitor (ta, tb, ty);
+      ta = 1'b0;
+      tb = 1'b0;
+      #10
+      ta = 1'b0;
+      tb = 1'b1;
+      #10
+      ta = 1'b1;
+      tb = 1'b0;
+      #10
+      ta = 1'b1;
+      tb = 1'b1;
+    end
+endmodule
